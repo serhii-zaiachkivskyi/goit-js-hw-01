5710c3e3d382ca970f87807991d5b1df9ee3580b
Serhii Zaiachkivskyi <serhiy0102@gmail.com>
2025-08-13 19:41:15 +0200

    add first js task-3

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     content = parseFloat(content);
     padding = parseFloat(padding);
     border = parseFloat(border);
-    return (`${content + padding * 2 + border * 2}`);
+    return content + padding * 2 + border * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
